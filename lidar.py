caadcad85cddd74ade9ddfc76cb1a9aa340a286c
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-02 19:14:19 -0800

    first version of sricpt

--- lidar.py
+++ lidar.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
 import time
+import json
+import signal
 from UDPComms import Publisher
 
 from rplidar import RPLidar
 
-
-fields = "t " *240
-format_ = "240f"
-port = 8110
-
-pub = Publisher(fields, format_, port)
-
+pub = Publisher("json",  "2048s", 8110)
 lidar = RPLidar('/dev/ttyUSB0')
 
+def signal_term_handler(signal, frame):
+    lidar.stop()
+    lidar.stop_motor()
+    exit()
+ 
+signal.signal(signal.SIGTERM, signal_term_handler)
 
 for scan in lidar.iter_scans():
-    scan[1]
+    pub.send(bytes(json.dumps(scan), 'utf-8'))
 

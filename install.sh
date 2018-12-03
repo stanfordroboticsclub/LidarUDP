e5053134e6da2147f9856a4dafbab400a6ef94a0
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-02 16:33:05 -0800

    added install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+FOLDER=$(dirname $(realpath "$0"))
+cd $FOLDER
+
+yes | sudo pip3 install rplidar-roboticia
+
+for file in *.service; do
+    [ -f "$file" ] || break
+    sudo ln -s $FOLDER/$file /lib/systemd/system/
+done
+
+sudo systemctl daemon-reload

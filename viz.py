959e8fb4ff7a76e24e8e21712a10fa76549b8f1f
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-12-16 03:36:08 -0800

    added fucntion layout

--- viz.py
+++ viz.py
@@ -5,6 +5,7 @@ import math
 import time
 
 
+RATE = 100
 WINDOW_SIDE = 1000
 MM_PER_PIX = 4
 
@@ -14,16 +15,22 @@ MM_PER_INCH =2.54
 class Map:
 
     def __init__(self,map_topic, world_frame):
-        self.x_size_m = 40
+        self.x_size_m = 40 # chagne to mm?
         self.y_size_m = 40
         self.resolution = 0.05
 
         self.x_size_g = int(self.x_size_m/self.resolution)
         self.y_size_g = int(self.y_size_m/self.resolution)
 
-        self.map = [ float("nan") ] * self.y_size_g*self.x_size_g
+        self.map = [ [float("nan")]*self.y_size_g for _ in range(self.x_size_g)] 
 
         # self.map_pub = UDPComms.Publisher(8888)
+
+    def up(self,key):
+        pass
+
+    def down(self, key):
+        x = int((key[0] + self.x_size_m/2)/self.resolution)
 
     def __getitem__(self, key):
         x = int((key[0] + self.x_size_m/2)/self.resolution)
@@ -43,12 +50,21 @@ class Map:
 
     def publish_map(self):
         pass
+
+    def interpolate(self):
+        pass
+
+    def interpolate_derivative(self):
+        pass
+
+    def scan_match(self):
+        pass
         
 
 class Robot:
     def __init__(self, l, r):
 
-        # self.sub = subscriber
+        self.odom = UDPComms.Subscriber("l r",  "ff", 8820, timeout = 2)
 
         # self.th measured CCW from X axis
         self.x = 0
@@ -57,20 +73,25 @@ class Robot:
 
         self.WHEEL_RAD = MM_PER_INCH * 6.5/2 #mm
         self.WHEEL_BASE = MM_PER_INCH * 18  #mm
-        self.wheel_l, self.wheel_r = l, r #rotations
+        self.wheel_l, self.wheel_r = self.odom.recv() #rotations
 
         self.lidar_offset_forward = MM_PER_INCH * 18
 
     def get_pose(self):
         return (self.x, self.y, self.th)
 
-    def convert_lidar_to_map(self, angle, dist):
+    def set_pose(self, x, y, th):
+        self.x  = x
+        self.y  = y
+        self.th = th
+
+    def lidar_to_map(self, angle, dist):
         return (self.x + dist * math.cos(angle + self.th) + self.lidar_offset_forward * math.cos(self.th), 
                 self.y + dist * math.sin(angle + self.th) + self.lidar_offset_forward * math.sin(self.th))
 
-    def update(self, l ,r):
+    def update_odom(self):
         # import pdb; pdb.set_trace()
-        # l, r = self.sub.get()
+        l, r = self.odom.get()
 
         # http://faculty.salina.k-state.edu/tim/robotics_sg/Control/kinematics/odometry.html
 
@@ -91,14 +112,13 @@ class Robot:
         self.y += delta_y;
         self.th += v_th;
 
-        print( self.x, self.y, self.th)
+        print(self.x, self.y, self.th)
 
 
 
 sub = UDPComms.Subscriber("data",  "4096s", 8110, 1)
-odom = UDPComms.Subscriber("l r",  "ff", 8820, timeout = 2)
-rob = Robot( *odom.get() )
 
+rob = Robot( *odom.get() )
 
 class LidarWindow:
     def __init__(self):
@@ -108,60 +128,51 @@ class LidarWindow:
         self.canvas = tk.Canvas(self.root,width=WINDOW_SIDE,height=WINDOW_SIDE)
         self.canvas.pack()
 
+        self.arrow = canvas.create_line(0, 0, SIDE, SIDE, arrow=tk.LAST)
 
-        arrow_length = 10
-        arrow = canvas.create_line(0, 0, SIDE, SIDE, arrow=tk.LAST)
-
-        time.sleep(2)
-
-        self.root.after(100,update)
+        self.root.after(RATE,update)
         self.root.mainloop()
 
-
     def create_point(self,x,y):
-        self.canvas.create_oval(x, y, x, y, width = 1, fill = '#000000')
+        return self.canvas.create_oval(x, y, x, y, width = 1, fill = '#000000')
 
-    def convert_map_to_cavnas(self,in_x, in_y):
-        y= SIDE/2 - in_x / WINDOW_SIDE
-        x= SIDE/2 + in_y / WINDOW_SIDE
+    def create_pose(self,x,y, th):
+        return canvas.create_line(x, y, x + 10*math.sin(th),
+                                        y - 10*math.cos(th), arrow=tk.LAST)
+
+    def to_canvas(self,x, y):
+        y= WINDOW_SIDE/2 - x / MM_PER_PIX
+        x= WINDOW_SIDE/2 + y / MM_PER_PIX
         return (x,y)
 
-def update():
-    global arrow
-    try:
+    def update(self):
+        try:
+
+            rob.update_odom()
+            pose = sdf.scan_match()
+            rob.set_pose(*pose)
+
+            unpacker = msgpack.Unpacker()
+            unpacker.feed(sub.get().data)
+            data = unpacker.unpack()
+
+            rob_x, rob_y = self.to_canvas(rob.x, rob.y)
+
+            canvas.delete(self.arrow)
+            self.arrow = canvas.pose(*self.to_canvas(rob.x, rob.y), rob.th )
+            
+            # canvas.delete('all')
+
+            for _, angle, dist in data:
+                a = math.radians(angle)
+
+                point_x, point_y = rob.lidar_to_map(a, dist)
+                create_point( *self.to_canvas(point_x, point_y ) )
+            print()
 
 
-        rob.update(  *odom.get())
-
-        unpacker = msgpack.Unpacker()
-        unpacker.feed(sub.get().data)
-        data = unpacker.unpack()
-        
-
-        rob_x, rob_y = convert_map_to_cavnas(rob.x * MM_PER_INCH, rob.y * MM_PER_INCH)
-
-        canvas.delete(arrow)
-        arrow = canvas.create_line(rob_x, rob_y, rob_x + arrow_length*math.sin(rob.th),
-                                         rob_y - arrow_length*math.cos(rob.th), arrow=tk.LAST)
-        
-        # canvas.delete('all')
-        # canvas.create_oval(255, 255, 245, 245, fill = '#FF0000')
-
-
-        print("Robot loc: ", rob_x, rob_y)
-
-        for _, angle, dist in data:
-            a = math.radians(angle)
-
-
-            point_x, point_y = convert_lidar_to_map( rob.x * MM_PER_INCH , rob.y * MM_PER_INCH, rob.th, a, dist/ SCALE )
-            # print(angle,dist, point_x, point_y)
-            create_point( *convert_map_to_cavnas(point_x, point_y ) )
-        print()
-
-
-    finally:
-        r.after(100,update)
+        finally:
+            r.after(RATE,update)
 
 
 
